2dab8986017560516f82a6e31aab434dace8781c
Sani Abubakar <egbaojo@gmail.com>
2017-08-27 09:18:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
-var playlist = {Steve Wonder: "Here I'am"}
+var playlist = {Phil Ochs: "Here I'am"};
+function updatePlaylist(playlist, artistName, songTitle) {
+  var ply = object.assign({}, playlist, Phil Ochs: "Here's to the State of Mississippi")
+  return ply
+}
