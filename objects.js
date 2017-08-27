f8192669cdb9288bce3a55d7d18ffcc9c8b6e18e
Sani Abubakar <egbaojo@gmail.com>
2017-08-27 10:23:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 playlist['Phil Ochs'] = "Here's to the State of Mississippi"
 return playlist
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye
+  return playlist
+}
