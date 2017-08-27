c97b31a9f3667eb0c66c801ebd07805048305386
Sani Abubakar <egbaojo@gmail.com>
2017-08-27 09:42:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,1 @@
-var playlist = {Phil Ochs: 'Here I come'}
-function updatePlaylist(playlist, artistName, songTitle) {
-  object.assign(playlist, Phil Ochs:"Here's to the State of Mississippi")
-  return bject.assign(playlist, Phil Ochs:"Here's to the State of Mississippi")
-}
+var playlist = {Bobmale: 'Home I come', Lucky: 'Am working away'}
