a1925ab460fe630f4bbb494160eb4346edd57513
Sani Abubakar <egbaojo@gmail.com>
2017-08-27 10:11:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Bobmale: 'Home I come', Lucky: 'Am working away', Kanye: 'Gold Digger'}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
-  return playlist['Phil Ochs']
+  return object.assign({}, playlist)
 }
